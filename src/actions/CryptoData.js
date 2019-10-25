a25ad792e2bc989c76f1a1f09c39e09ef8530d04
Kyle Leonard <kdleonard@icloud.com>
2019-10-24 20:42:54 -0500

    Attempting to set up my axios actions

---
+++ src/actions/CryptoData.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const FETCH_CRYPTO_START = "FETCH_CRYPTO_START";
+export const FETCH_CRYPTO_SUCCESS = "FETCH_CRYPTO_SUCCESS";
+export const FETCH_CRYPTO_ERROR = "FETCH_CRYPTO_ERROR";
+
+export function fetchDog() {
+  return dispatch => {
+    dispatch({ type: FETCH_CRYPTO_START });
+
+    axios
+      .get(`https://api.coinlore.com/api/ticker/?${props.newCoin}`)
+      .then(res => {
+        // enter the "success" state
+        dispatch({ type: FETCH_CRYPTO_SUCCESS, payload: res.data });
+      })
+      .catch(err => {
+        // enter the "error" state
+        dispatch({ type: FETCH_CRYPTO_ERROR, payload: err });
+      });
+  };
+}
