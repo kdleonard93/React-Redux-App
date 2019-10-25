1da310152d45e0a6168b2a30e2f9141ef4bbc72c
Kyle Leonard <kdleonard@icloud.com>
2019-10-24 21:19:26 -0500

    Imported fetchCrypto action

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./App.sass";
+import { fetchCrypto } from "./actions/CryptoData";
 import HeroHeading from "./components/Hero";
 
 function App(props) {
+  useEffect(() => {
+    props.fetchCrypto();
+  }, [props]);
+
   return (
     <div>
       <HeroHeading />
